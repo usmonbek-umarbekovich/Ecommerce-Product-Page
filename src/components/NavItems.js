dabc2f80291365e123756625f41dd9f1987f0e9d
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2021-10-30 23:26:17 +0500

    ceated NavItems component

---
+++ src/components/NavItems.js
@@ -0,0 +1,23 @@
+function NavItems() {
+  return (
+    <ul>
+      <li>
+        <button>Collections</button>
+      </li>
+      <li>
+        <button>Men</button>
+      </li>
+      <li>
+        <button>Women</button>
+      </li>
+      <li>
+        <button>About</button>
+      </li>
+      <li>
+        <button>Contact</button>
+      </li>
+    </ul>
+  );
+}
+
+export default NavItems;
