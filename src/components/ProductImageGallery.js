ee8e5a1b6be7015f29797aac04ec0d22bd9947a7
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2021-10-30 23:50:42 +0500

    added lightbox gallery

--- src/components/ProductImageGallery.js
+++ src/components/ProductImageGallery.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
+import closeIcon from '../images/icon-close.svg';
+import nextIcon from '../images/icon-next.svg';
+import previousIcon from '../images/icon-previous.svg';
 
-function ProductImageGallery({ images }) {
+function ProductImageGallery({ images, lightbox }) {
   const [index, setIndex] = useState(1);
 
   const thumbnails = Object.keys(images).filter(image =>
@@ -11,6 +14,19 @@ function ProductImageGallery({ images }) {
     <div>
       <div className='main-image'>
         <img src={images[`image-product-${index}.jpg`]} alt='Product' />
+        {lightbox && (
+          <>
+            <svg className='close-gallery'>
+              <use href={`${closeIcon}#close`} />
+            </svg>
+            <svg className='previous-image'>
+              <use href={`${previousIcon}#previous`} />
+            </svg>
+            <svg className='next-image'>
+              <use href={`${nextIcon}#next`} />
+            </svg>
+          </>
+        )}
       </div>
       <ul className='thumbnail-container'>
         {thumbnails.map((image, i) => (
