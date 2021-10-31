4177c1017192b3c590ed96a8b66278cc4f795758
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2021-11-01 02:30:31 +0500

    fixed bug, implemented deleting from cart

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,27 +1,31 @@
 import productImage from '../images/image-product-1-thumbnail.jpg';
 import removeIcon from '../images/icon-delete.svg';
+import { useProducts } from '../contexts/ProductsProvider';
 
 function CartItem({ product }) {
+  const { deleteProduct } = useProducts();
   const { name, count, price, discount } = product;
   const discountedPrice = price * (1 - discount);
 
   return (
     <li>
-      <div className='product-image'>
+      <div className='cart-product-image'>
         <img src={productImage} alt='Product' />
       </div>
-      <div className='product-info'>
-        <p className='product-name'>{name}</p>
-        <p className='product-price'>
+      <div className='cart-product-info'>
+        <p className='cart-product-name'>{name}</p>
+        <p className='cart-product-price'>
           ${discountedPrice.toFixed(2)} x {count}{' '}
           <span className='total-price'>
             ${(discountedPrice * count).toFixed(2)}
           </span>
         </p>
       </div>
-      <button className='btn remove-product'>
-        <img src={removeIcon} alt='Remove' />
-      </button>
+      <svg
+        className='btn remove-product'
+        onClick={() => deleteProduct(product)}>
+        <use href={`${removeIcon}#delete`} />
+      </svg>
     </li>
   );
 }
