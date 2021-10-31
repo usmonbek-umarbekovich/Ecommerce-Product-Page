fcd6aa4d91f55fa5aa2680ad31880bc5edad74ef
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2021-11-01 02:29:41 +0500

    styled lightbox & added event handlers

--- src/components/ProductImageGallery.js
+++ src/components/ProductImageGallery.js
@@ -3,34 +3,64 @@ import closeIcon from '../images/icon-close.svg';
 import nextIcon from '../images/icon-next.svg';
 import previousIcon from '../images/icon-previous.svg';
 
-function ProductImageGallery({ images, lightbox }) {
+function ProductImageGallery(props) {
   const [index, setIndex] = useState(1);
+  const { images, lightbox, onOpenGallery, onCloseGallery } = props;
 
   const thumbnails = Object.keys(images).filter(image =>
     image.includes('thumbnail')
   );
 
+  function handleOpenGallery() {
+    if (lightbox) return;
+
+    onOpenGallery();
+  }
+
+  function handlePreviousImage() {
+    setIndex(prev => {
+      if (prev === 1) return thumbnails.length;
+      else return prev - 1;
+    });
+  }
+
+  function handleNextImage() {
+    setIndex(prev => {
+      if (prev === thumbnails.length) return 1;
+      else return prev + 1;
+    });
+  }
+
   return (
-    <div>
-      <div className='main-image'>
+    <div className={`image-gallery ${lightbox ? 'lightbox' : ''}`}>
+      <div className='main-image' onClick={handleOpenGallery}>
         <img src={images[`image-product-${index}.jpg`]} alt='Product' />
         {lightbox && (
           <>
-            <svg className='close-gallery'>
+            <svg className='close-gallery' onClick={onCloseGallery}>
               <use href={`${closeIcon}#close`} />
             </svg>
-            <svg className='previous-image'>
-              <use href={`${previousIcon}#previous`} />
-            </svg>
-            <svg className='next-image'>
-              <use href={`${nextIcon}#next`} />
-            </svg>
+            <div className='previous-image' onClick={handlePreviousImage}>
+              <svg>
+                <use href={`${previousIcon}#previous`} />
+              </svg>
+            </div>
+            <div className='next-image' onClick={handleNextImage}>
+              <svg>
+                <use href={`${nextIcon}#next`} />
+              </svg>
+            </div>
           </>
         )}
       </div>
       <ul className='thumbnail-container'>
         {thumbnails.map((image, i) => (
-          <li key={i} onClick={() => setIndex(i + 1)}>
+          <li
+            key={i}
+            onClick={() => setIndex(i + 1)}
+            className={`thumbnail ${
+              index === i + 1 ? 'active-thumbnail' : ''
+            }`}>
             <img src={images[image]} alt='Product Thumbnail' />
           </li>
         ))}
